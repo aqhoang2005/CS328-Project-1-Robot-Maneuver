b317220bc168d90b2c890639f6c1e2c5e7307091
Austin Hoang <neo322442@gmail.com>
2026-03-30 16:10:47 -0400

    Halt, Accelerate, Turn
    New functions added to handle smoother turning, stopping, and acceleration of the robot car. Old code commented just in case if we needed.

--- sketch_mar26b/sketch_mar26b.ino
+++ sketch_mar26b/sketch_mar26b.ino
@@ -112,6 +112,34 @@ void StopMotors() {
   digitalWrite(INA2B, LOW);
 }
 
+void Halt(int startSpeed)
+{
+  for (int s = startSpeed; s > 0; s -= 20) {
+    Forward(s);
+    delay(20);
+  }
+    StopMotors();
+  
+}
+
+void Turn(int maxSpeed, int turnTime)
+{
+  for (int s = 80; s <= maxSpeed; s += 20) {
+    Right(s);
+    delay(20);
+  }
+  delay(turnTime);  // actual turning duration
+  StopMotors();
+}
+
+void Accelerate(int maxSpeed)
+{
+  for (int s = 80; s <= maxSpeed; s += 20) {
+    Forward(s);
+    delay(20);
+  }
+}
+
 //Setup function.
 void setup() {
   Serial.begin(9600);
@@ -163,75 +191,109 @@ void loop() {
     }
     else if (cmd == 'Q'){ //these were all calibrated using a dead ish battery
       int speed = 200;
-
       Forward(speed);
-      delay(500);//
-      StopMotors();
+      delay(330);
       
-      delay(100);
-
-      Right(speed);
-      digitalWrite(RIGHTREAR, HIGH);
-      digitalWrite(RIGHTFRONT, HIGH);
-      delay(160);// delay was 160 super charged, delay was 180 when charged, 200 when dead ish
-      digitalWrite(RIGHTREAR, LOW);
-      digitalWrite(RIGHTFRONT, LOW);
-      StopMotors();
-      digitalWrite(LEFTREAR, HIGH);
-      digitalWrite(RIGHTREAR, HIGH);
-
-      delay(100);//new 0
-      digitalWrite(LEFTREAR, LOW);
-      digitalWrite(RIGHTREAR, LOW);
-
-      Forward(speed);
-      delay(500);// 400 dead, 600 alive
-      StopMotors();
-
-      delay(100);//new 2 and below
-
-      Right(speed);
-      digitalWrite(RIGHTREAR, HIGH);
-      digitalWrite(RIGHTFRONT, HIGH);
-      delay(240);//was 240
-      StopMotors();
-
-      delay(100);//new 3
-
-      Forward(speed);
-      delay(600);//300 dead, 400 alive was 500
-      StopMotors();
-      digitalWrite(RIGHTREAR, LOW);
-      digitalWrite(RIGHTFRONT, LOW);
-      StopMotors();
-      digitalWrite(LEFTREAR, HIGH);
-      digitalWrite(RIGHTREAR, HIGH);
-
-      delay(100);//new 4 and below
-
-      Right(speed);
-      digitalWrite(RIGHTFRONT, HIGH);
-      digitalWrite(RIGHTREAR, HIGH);
-      delay(180);
-      StopMotors();
-      digitalWrite(RIGHTREAR, LOW);
-      digitalWrite(RIGHTFRONT, LOW);
-      StopMotors();
-      digitalWrite(LEFTREAR, HIGH);
-      digitalWrite(RIGHTREAR, HIGH);
-
-      delay(100);
-
-      Forward(speed);
+      Halt(speed);
       delay(500);
-      StopMotors();
-      digitalWrite(RIGHTREAR, LOW);
-      digitalWrite(RIGHTFRONT, LOW);
-      StopMotors();
-      digitalWrite(LEFTREAR, HIGH);
-      digitalWrite(RIGHTREAR, HIGH);
-
+      Turn(speed, 100);
       delay(100);
+      Accelerate(speed);
+      delay(320);
+
+      Halt(speed);
+      delay(500);
+      Turn(speed, 80);
+      delay(100);
+      // Accelerate(speed);      
+      // delay(330);
+
+      // Halt(speed);
+      // delay(500);
+      // Turn(speed, 100);
+      // delay(100);
+      // Accelerate(speed);      
+      // delay(320);
+
+      // Halt(speed);
+      // delay(500);
+      // Turn(speed, 100);
+      // delay(100);
+      // Accelerate(speed);      
+      // delay(340);
+
+      // Halt(speed);
+
+      /*Old Code in case*/
+
+      // Forward(speed);
+      // delay(500);//
+      // StopMotors();
+      
+      // delay(100);
+
+      // Right(speed);
+      // digitalWrite(RIGHTREAR, HIGH);
+      // digitalWrite(RIGHTFRONT, HIGH);
+      // delay(160);// delay was 160 super charged, delay was 180 when charged, 200 when dead ish
+      // digitalWrite(RIGHTREAR, LOW);
+      // digitalWrite(RIGHTFRONT, LOW);
+      // StopMotors();
+      // digitalWrite(LEFTREAR, HIGH);
+      // digitalWrite(RIGHTREAR, HIGH);
+
+      // delay(100);//new 0
+      // digitalWrite(LEFTREAR, LOW);
+      // digitalWrite(RIGHTREAR, LOW);
+
+      // Forward(speed);
+      // delay(500);// 400 dead, 600 alive
+      // StopMotors();
+
+      // delay(100);//new 2 and below
+
+      // Right(speed);
+      // digitalWrite(RIGHTREAR, HIGH);
+      // digitalWrite(RIGHTFRONT, HIGH);
+      // delay(240);//was 240
+      // StopMotors();
+
+      // delay(100);//new 3
+
+      // Forward(speed);
+      // delay(600);//300 dead, 400 alive was 500
+      // StopMotors();
+      // digitalWrite(RIGHTREAR, LOW);
+      // digitalWrite(RIGHTFRONT, LOW);
+      // StopMotors();
+      // digitalWrite(LEFTREAR, HIGH);
+      // digitalWrite(RIGHTREAR, HIGH);
+
+      // delay(100);//new 4 and below
+
+      // Right(speed);
+      // digitalWrite(RIGHTFRONT, HIGH);
+      // digitalWrite(RIGHTREAR, HIGH);
+      // delay(180);
+      // StopMotors();
+      // digitalWrite(RIGHTREAR, LOW);
+      // digitalWrite(RIGHTFRONT, LOW);
+      // StopMotors();
+      // digitalWrite(LEFTREAR, HIGH);
+      // digitalWrite(RIGHTREAR, HIGH);
+
+      // delay(100);
+
+      // Forward(speed);
+      // delay(500);
+      // StopMotors();
+      // digitalWrite(RIGHTREAR, LOW);
+      // digitalWrite(RIGHTFRONT, LOW);
+      // StopMotors();
+      // digitalWrite(LEFTREAR, HIGH);
+      // digitalWrite(RIGHTREAR, HIGH);
+
+      // delay(100);
     }
   }
 }
