28cec4bfbc6bcfbdcba07027e1159dc832cad758
Austin Hoang <neo322442@gmail.com>
2026-04-02 15:50:12 -0400

    tweaked timers
    Tweaked timers turning and halting.

--- sketch_mar26b/sketch_mar26b.ino
+++ sketch_mar26b/sketch_mar26b.ino
@@ -1,10 +1,227 @@
-void setup() {
-  // put your setup code here, to run once:
+//From LAB 5 PWM Motor.c.txt
+// Pins for all inputs, keep in mind the PWM defines must be on PWM pins
+#define MotorPWM_A 4 //left motor
+#define MotorPWM_B 5 //right motor
+#define BLUETOOTH_BAUD_RATE 38400
 
+// Motor pins
+#define MotorPWM_L 4   // left motor PWM
+#define MotorPWM_R 5   // right motor PWM
+#define INA1A 32
+#define INA2A 34
+#define INA1B 30
+#define INA2B 36
+
+//LED pins
+#define LEFTREAR 31
+#define LEFTFRONT 49
+#define RIGHTREAR 37
+#define RIGHTFRONT 43
+
+// Encoder pins
+#define ENCODER_LEFT  2
+#define ENCODER_RIGHT 3
+
+// ============================
+// Encoder counters
+// ============================
+volatile long count_left = 0;
+volatile long count_right = 0;
+
+// ============================
+// RPM conversion
+// For 100 ms sample time:
+// RPM = counts * 3.125
+// because 60/0.1 / (48*4) = 3.125
+// ============================
+float rotation = 3.125;
+int pwm = 0;
+
+// sample time in ms
+const unsigned long sampleTime = 100;
+
+// ============================
+// Interrupt service routines
+// ============================
+void ISRMotorLeft() {
+  count_left++;
+}
+
+void ISRMotorRight() {
+  count_right++;
+}
+// ============================
+// Light control
+// ============================
+void OFFLights(){
+  digitalWrite(LEFTREAR, LOW);
+  digitalWrite(RIGHTREAR, LOW);
+  digitalWrite(LEFTFRONT, LOW);
+  digitalWrite(RIGHTFRONT, LOW);  
+}
+
+void ONLights(){
+  digitalWrite(LEFTREAR, HIGH);
+  digitalWrite(RIGHTREAR, HIGH);
+  digitalWrite(LEFTFRONT, HIGH);
+  digitalWrite(RIGHTFRONT, HIGH);
+}
+
+void BREAKLights(){
+  digitalWrite(LEFTREAR, HIGH);
+  digitalWrite(RIGHTREAR, HIGH);
+}
+
+void RIGHTLights(){
+  digitalWrite(RIGHTREAR, HIGH);
+  digitalWrite(RIGHTFRONT, HIGH);
+}
+
+void LEFTLights(){
+  digitalWrite(LEFTREAR, HIGH);
+  digitalWrite(LEFTFRONT, HIGH);
+}
+
+// ============================
+// Motor control
+// ============================
+void Forward(int speed) {
+  analogWrite(MotorPWM_L, speed);
+  analogWrite(MotorPWM_R, speed);
+
+  // Left motor forward
+  digitalWrite(INA1A, HIGH);
+  digitalWrite(INA2A, LOW);
+
+  // Right motor forward
+  digitalWrite(INA1B, HIGH);
+  digitalWrite(INA2B, LOW);
+}
+
+void Backward(int speed) {
+  analogWrite(MotorPWM_L, speed);
+  analogWrite(MotorPWM_R, speed);
+
+  // Left motor backward
+  digitalWrite(INA1A, LOW);
+  digitalWrite(INA2A, HIGH);
+
+  // Right motor backward
+  digitalWrite(INA1B, LOW);
+  digitalWrite(INA2B, HIGH);
+}
+
+void Left(int speed) 
+{
+  OFFLights();
+  LEFTLights();
+  analogWrite(MotorPWM_L, speed);
+  analogWrite(MotorPWM_R, speed - 20);
+  
+  // Left motor backward
+  digitalWrite(INA1A, LOW);
+  digitalWrite(INA2A, HIGH);
+
+  // Right motor forward
+  digitalWrite(INA1B, HIGH);
+  digitalWrite(INA2B, LOW);
+}
+
+void Right(int speed)
+{
+  OFFLights();
+  RIGHTLights();
+  analogWrite(MotorPWM_L, speed - 20);
+  analogWrite(MotorPWM_R, speed);
+  
+  // Left motor forward
+  digitalWrite(INA1A, HIGH);
+  digitalWrite(INA2A, LOW);
+
+  // Right motor backward
+  digitalWrite(INA1B, LOW);
+  digitalWrite(INA2B, HIGH);
+}
+
+void StopMotors() {
+  OFFLights();
+  ONLights();
+  analogWrite(MotorPWM_L, 0);
+  analogWrite(MotorPWM_R, 0);
+
+  digitalWrite(INA1A, LOW);
+  digitalWrite(INA2A, LOW);
+  digitalWrite(INA1B, LOW);
+  digitalWrite(INA2B, LOW);
+}
+
+void Halt(int startSpeed)
+{
+  BREAKLights();
+  for (int s = startSpeed; s > 0; s -= 20) {
+    Forward(s);
+    delay(20);
+  }
+    StopMotors();
+  
+}
+
+void Turn(int maxSpeed, int turnTime)
+{
+  for (int s = 80; s <= maxSpeed; s += 20) {
+    Right(s);
+    delay(20);
+  }
+  delay(turnTime);  // actual turning duration
+  StopMotors();
+}
+
+void Accelerate(int maxSpeed)
+{
+  for (int s = 80; s <= maxSpeed; s += 20) {
+    Forward(s);
+    delay(20);
+  }
+}
+
+//Setup function.
+void setup() {
+  Serial.begin(9600);
+  Serial2.begin(BLUETOOTH_BAUD_RATE);
+
+  // Motor pins
+  pinMode(MotorPWM_L, OUTPUT);
+  pinMode(MotorPWM_R, OUTPUT);
+  pinMode(INA1A, OUTPUT);
+  pinMode(INA2A, OUTPUT);
+  pinMode(INA1B, OUTPUT);
+  pinMode(INA2B, OUTPUT);
+
+  // Encoder pins
+  pinMode(ENCODER_LEFT, INPUT_PULLUP);
+  pinMode(ENCODER_RIGHT, INPUT_PULLUP);
+
+  // Light Control
+  pinMode(LEFTFRONT, OUTPUT);
+  pinMode(RIGHTFRONT, OUTPUT);
+  pinMode(LEFTREAR, OUTPUT);
+  pinMode(RIGHTREAR, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(ENCODER_LEFT), ISRMotorLeft, FALLING);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_RIGHT), ISRMotorRight, FALLING);
+
+  StopMotors();
+  delay(1000);
+
+  Serial.println("PWM,RPM_Left,RPM_Right");
+  Serial2.println("PWM,RPM_Left,RPM_Right");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+    if (Serial2.available()) {
+    char cmd = Serial2.read();
+
     if (cmd == 'F') {
       Forward(128);
     }
@@ -27,33 +244,27 @@ void loop() {
       
       Halt(speed); //First turn
       delay(500);
-      Turn(speed, 80);
+      Turn(speed, 85);
       delay(105);
       Accelerate(speed);
-      delay(320);
+      delay(300); //ms
 
       Halt(speed); //Second turn
       delay(500);
-      Turn(speed, 80);
-      delay(110);
+      Turn(speed, 85);
+      delay(105);
       Accelerate(speed);      
-      delay(310);
+      delay(280);
 
-      Halt(speed);
+      Halt(speed); //Third turn
       delay(500);
-      Turn(speed, 80);
-      delay(100);
+      Turn(speed, 85);
+      delay(105);
       Accelerate(speed);      
-      delay(320);
+      delay(280);
 
-      Halt(speed); //Fourth turn
-      // delay(500);
-      // Turn(speed, 100);
-      // delay(100);
-      // Accelerate(speed);      
-      // delay(340);
-
-      // Halt(speed);
+      Halt(speed); //Stops at beginning point (hopefully.)
+      StopMotors();
 
       /*Old Code in case*/
 
@@ -127,3 +338,4 @@ void loop() {
       // delay(100);
     }
   }
+}
